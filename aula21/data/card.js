f4ee9b5113dcfda77173368f6b1e44338fcf2e67
TaináVilela <tainadantas89@hotmail.com>
2021-01-22 17:42:28 -0300

    corrigindo erros

---
+++ aula21/data/card.js
@@ -0,0 +1,29 @@
+var cards = [
+    {
+        'caminho_de_imagem': "/images/lasanhaAbobrinha.jpg",
+        'titulo':"Carnes e Massas",
+        'texto':"Cozinha simples, com sabor e qualidade.",
+        'botao': "Ver mais"
+    },    
+    {
+        'caminho_de_imagem':"/images/paoCalabresaCortado.jpeg",
+        'titulo': "Pães e Bolos",
+        'texto': "Pães e Bolos, com receitas que lembram a infância.",
+        'botao': "Ver mais"
+    },
+    {
+        'caminho_de_imagem': "/images/pudim.jpg",
+        'titulo': "Doces e Sobremesas",
+        'texto': "Sobremesas que adoçam a boca e o coração.",
+        'botao': "Ver mais"
+    },
+    {
+     'caminho_de_imagem': "/images/IMG_20200927_210523.jpg" ,
+     'titulo': "Harmonização",
+     'texto': "Vinhos, Cervejas, Drinks para uma experiência gastronômica completa.",
+     'botao': "Ver mais" 
+    }
+];
+
+module.exports = cards;
+
