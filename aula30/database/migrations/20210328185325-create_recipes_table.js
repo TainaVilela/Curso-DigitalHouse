5d69db71faf3c83160268ccd315db6330432345c
TaináVilela <tainadantas89@hotmail.com>
2021-04-07 19:55:35 -0300

    criando banco de recitas e usuarios

---
+++ aula30/database/migrations/20210328185325-create_recipes_table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('recipes', { 
+       id: {
+         type: Sequelize.INTEGER, 
+         primaryKey: true,
+         autoIncremente: true,
+      },
+      name: Sequelize.STRING,
+      email: Sequelize.STRING,
+      message: Sequelize.STRING,
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+      
+    });
+     
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('recipes');
+   
+  }
+};
---
+++ aula31/controllers/RecipeController.js
@@ -0,0 +1,52 @@
+const { Recipe } = require('../models');
+
+// 1ª forma que voces podem encontrar de exportar metodos do controller
+module.exports = {
+  async index(req, res, next){
+    let recipes = await Recipe.findAll();
+    res.render('recipes', { recipes, user: req.session.user });
+  },
+
+  async save(req, res, next){
+    let recipe = { ...req.body };
+    await Recipe.create(recipe);
+
+    res.redirect('/recipes');
+  },
+
+  async edit(req, res, next){
+    let id = req.params.id;
+
+    // obter a receita para altera-la
+    let recipe = await Recipe.findByPk(id);
+
+    res.render('edit-recipe', { recipe, user: req.session.user });
+  },
+
+  async update(req, res, next){
+    let id = req.params.id;
+    let { name, email, message } = req.body;
+
+    // obter a receita para altera-la
+    let recipe = await Recipe.findByPk(id);  
+    
+    recipe.update({
+      name,
+      email,
+      message
+    });
+
+    res.render('edit-recipe', { recipe, updated: true });
+  },
+
+  async delete(req, res, next){
+    let id = req.params.id;
+
+    // obter a receita para altera-la
+    let recipe = await Recipe.findByPk(id);  
+
+    recipe.destroy();
+
+    res.redirect('/recipes');
+  }
+}
